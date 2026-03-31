198c01346f6e2205992dc944656ea85a1eb46407
ha2m2 <hamin0691@gmail.com>
2026-03-31 16:00:56 +0900

    feat 모듈 인터페이스 구현

---
+++ Plugins/MyNBCLog/Source/MyNBCLog/MyNBCLog.cpp
@@ -0,0 +1,16 @@
+﻿#include "MyNBCLog.h"
+
+#define LOCTEXT_NAMESPACE "FMyNBCLogModule"
+
+void FMyNBCLogModule::StartupModule()
+{
+	UE_LOG(LogTemp, Warning, TEXT("MyNBCLog Plugin has started!"));
+}
+
+void FMyNBCLogModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FMyNBCLogModule, MyNBCLog)
